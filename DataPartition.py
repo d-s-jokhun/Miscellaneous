80230cb823a1f5577c5b5e21885bc00fe8286121
D. S. Jokhun <d.s.jokhun@gmail.com>
2020-08-06 17:06:53 +0800

    update

--- DataPartition.py
+++ DataPartition.py
@@ -5,28 +5,39 @@
 
 
 import numpy as np
-import math
 
 
-# In[41]:
+# In[1]:
 
 
 
-def DataPartition(FullSet, Partition=[0.6,0.2,0.2], RanSeed=None):
-    assert np.sum(Partition)<=1
+def DataPartition(Data2Partion_dict, Partition=[0.6,0.2,0.2], RanSeed=None):
+    assert np.sum(Partition)==1, 'Partition must be a list of fractions (Train_Frac, Val_Frac, Test_Frac) whose sum is equal to 1'
     np.random.seed(RanSeed)
-    l = len(FullSet)
-    new_order = np.random.choice(FullSet, size = l, replace = False)
-
-    Tr_MaxIdx = np.ceil(l*Partition[0]).astype(np.int)
-    Val_MaxIdx = np.ceil(l*(Partition[0]+Partition[1])).astype(np.int)
-    Ts_MaxIdx = np.ceil(l*(Partition[0]+Partition[1]+Partition[2])).astype(np.int)
-
-    Tr_Set = new_order[0:Tr_MaxIdx]
-    Val_Set = new_order[Tr_MaxIdx:Val_MaxIdx]
-    Ts_Set = new_order[Val_MaxIdx:Ts_MaxIdx]
     
-    return Tr_Set, Val_Set, Ts_Set
+    Ascd_ClassSizes = sorted([len(items[1]) for items in Data2Partion_dict.items()])
+    len_min = Ascd_ClassSizes[0]
+    len_max = Ascd_ClassSizes[-1]
+    
+    Test_size = round(Partition[2]*len_min)
+    Val_size = round(Partition[1]*len_min)
+    Train_size = len_max - (Val_size+Test_size)
+    
+    PartitionedData_dict = {}
+    for key in Data2Partion_dict.keys():
+        new_order = np.random.choice(Data2Partion_dict[key], size=len(Data2Partion_dict[key]), replace = False)
+        Tr_Set_temp=[]; Val_Set=[]; Ts_Set=[];
+        Ts_Set = new_order[:Test_size]
+        Val_Set = new_order[Test_size:Test_size+Val_size]
+        Tr_Set_temp = new_order[Test_size+Val_size:]
+        Tr_Set=[]
+        quotient=Train_size//len(Tr_Set_temp) ; remainder=Train_size%len(Tr_Set_temp)
+        for count in range(quotient):
+            Tr_Set.extend(Tr_Set_temp)
+        Tr_Set.extend(np.random.choice(Tr_Set_temp, size=remainder, replace = False))
+        PartitionedData_dict[key]={'Tr_Set':Tr_Set,'Val_Set':Val_Set,'Ts_Set':Ts_Set}
+        
+    return PartitionedData_dict
 
 
 # In[ ]:
